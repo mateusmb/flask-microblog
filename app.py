0f0e121793151674faff44df1677c314cd05ac1f
Mateus Bueno <mateusmb@192.168.1.12>
2022-05-12 11:12:03 -0300

    debugging env vars

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
+    print(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
 
 
